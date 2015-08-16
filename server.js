ca4b9889d2bb8a1396c1b24c1e53301c22aa2bae
Anees Ahee <aneesahee@gmail.com>
2015-08-16 14:30:49 +0100

    Serves non-functional upload page

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var jade = require('jade');
+
 var app = express();
+app.set('view engine', 'jade');
+app.set('views', './views');
+
 
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+	res.render('index');
+	//res.send('Hello World!');
 });
 
 var server = app.listen(1234, function() {
