262e5e16b49833240ec37d72ddd3059e14340808
Anees Ahee <aneesahee@gmail.com>
2015-08-27 01:46:30 +0100

    Image uploads working, but they aren't deleted.

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var jade = require('jade');
+var multer = require('multer');
+var storage = multer.diskStorage({
+		destination: function(req, file, cb) {
+			cb(null, './images/');
+		},
+		filename: function(req, file, cb) {
+			cb(null, Date.now() + '.png');
+		}
+})
+var upload = multer({storage: storage});
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', './views');
 
+app.use(express.static('images'));
 
 app.get('/', function(req, res) {
 	res.render('index');
-	//res.send('Hello World!');
+});
+
+app.post('/', upload.single('image'), function(req, res) {
+	var file = req.file;
+	console.log(file);
+	res.render('link', {file: file.filename})
 });
 
 var server = app.listen(1234, function() {
